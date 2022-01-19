a6a5e1a6ccbe2f7bf69303c371c7b7a030c3e87f
khuzama98 <skhuzamashahid@gmail.com>
2022-01-19 12:54:25 +0500

    updated rococo code

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -5,5 +5,21 @@ export const tokens = {
             new : 10,
             old: 12
         }
+    },
+    WESTEND: {
+        name: "WND",
+        decimals: 12
+    },
+    DUSTY: {
+        name: "PLD",
+        decimals: 15
+    },
+    SHIBUYA: {
+        name: "SBY",
+        decimals: 18
+    },
+    ROCOCO: {
+        name: "ROC",
+        decimals: 12
     }
 }
